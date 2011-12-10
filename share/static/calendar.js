2adad8bf549ec19c3829beda3904de69f4fea2aa
Hojung Youn <amoc.yn@gmail.com>
2011-12-10 12:06:52 +0900

    장식구 마우스 올리면 반짝

--- share/static/calendar.js
+++ share/static/calendar.js
@@ -2,4 +2,9 @@ $(window).load(function(){
     window.setInterval(function(){
         $("#ball-today").toggleClass("today");
     }, 900);
+    $(".ball").hover(function(en){
+        $(this).addClass("today");
+    }, function(){
+        $(this).removeClass("today");
+    });
 });
