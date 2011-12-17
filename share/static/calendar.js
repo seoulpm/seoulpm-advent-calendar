61fabe49f5bb162462e1d818f834516b90aa1186
Hojung Youn <amoc.yn@gmail.com>
2011-12-17 19:56:44 +0900

    툴팁 상자

--- share/static/calendar.js
+++ share/static/calendar.js
@@ -2,9 +2,15 @@ $(window).load(function(){
     window.setInterval(function(){
         $("#ball-today").toggleClass("today");
     }, 900);
-    $(".ball").hover(function(en){
+    $(".ball").hover(function(){
+        var offset = $(this).offset();
+        var tooltip = $("#tooltip");
         $(this).addClass("bling");
+        tooltip.css("left", offset.left - 280 + 50 + 9);
+        tooltip.css("top", offset.top - 86 - 15);
+        tooltip.css("display", "block");
     }, function(){
         $(this).removeClass("bling");
+        $("#tooltip").css("display", "none");
     });
 });
