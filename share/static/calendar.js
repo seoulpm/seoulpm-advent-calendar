fafe817d865d6032cbdb74653583e3946fb0772e
Hojung Youn <amoc.yn@gmail.com>
2011-12-17 20:36:16 +0900

    툴팁 상자 += 이미지

--- share/static/calendar.js
+++ share/static/calendar.js
@@ -1,3 +1,6 @@
+var adventcalendar = {
+    profile: [];
+};
 $(window).load(function(){
     window.setInterval(function(){
         $("#ball-today").toggleClass("today");
@@ -5,12 +8,15 @@ $(window).load(function(){
     $(".ball").hover(function(){
         var offset = $(this).offset();
         var tooltip = $("#tooltip");
+        var author = $(".author", $(this).parent()).text();
+        var title = $(".title", $(this).parent()).text();
         $(this).addClass("bling");
         tooltip.css("left", offset.left - 280 + 50 + 9);
         tooltip.css("top", offset.top - 86 - 15);
         tooltip.css("display", "block");
-        $("#tooltip .author").text($(".author", $(this).parent()).text());
-        $("#tooltip .title").text($(".title", $(this).parent()).text());
+        $("#tooltip .author").text(author);
+        $("#tooltip .title").text(title);
+        $("#tooltip .image").attr("src", "profile_" + author + ".jpg");
     }, function(){
         $(this).removeClass("bling");
         $("#tooltip").css("display", "none");
