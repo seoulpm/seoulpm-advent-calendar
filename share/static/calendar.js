57ec3f8e056369fd1ae11789a05901f884cdb0aa
Hojung Youn <amoc.yn@gmail.com>
2011-12-17 20:18:43 +0900

    툴팁 상자 += 정보 표시

--- share/static/calendar.js
+++ share/static/calendar.js
@@ -9,6 +9,8 @@ $(window).load(function(){
         tooltip.css("left", offset.left - 280 + 50 + 9);
         tooltip.css("top", offset.top - 86 - 15);
         tooltip.css("display", "block");
+        $("#tooltip .author").text($(".author", $(this).parent()).text());
+        $("#tooltip .title").text($(".title", $(this).parent()).text());
     }, function(){
         $(this).removeClass("bling");
         $("#tooltip").css("display", "none");
